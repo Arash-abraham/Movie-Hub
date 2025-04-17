2560325e403fe42588017046dd5164336c6a94c4
Arash <82562395+Arash-abraham@users.noreply.github.com>
2025-04-17 21:04:38 +0330

    Update Home.php

--- Application/Controllers/Home.php
+++ Application/Controllers/Home.php
@@ -1,11 +1,34 @@
 <?php
     namespace Application\Controllers;
+
+    use Application\Model\Category;
+    use Application\Model\Article;
+
     class Home extends Controller{
         public function index() {
-            $this->View('App.index');
+            $category = new Category();
+            $categories = $category->all();
+            $article = new Article();
+            $articles = $article->all();
+            return $this->View('App.index' , compact('categories' , 'articles'));
         }
-        public function test(){
-            print 'Hello from App\Controllers\Home test';
+        public function category($id) {
+            $ob_category = new Category();
+            $categories = $ob_category->all();
+            $ob_category = new Category();
+            $category = $ob_category->find($id);
+            $ob_category = new Category();
+            $articles = $ob_category->articles($id);
+            return $this->View('App.category' , compact('categories' , 'category' , 'articles'));
+
         }
+        public function show($id) {
+            $category = new Category();
+            $categories = $category->all();
+            $article = new Article();
+            $articles = $article->find($id);
+            return $this->View('App.show' , compact('categories' , 'articles'));
+        }
+
     }
 ?>
