006e66ed83f667d22d75ed1cee49ccbecad281d3
Arash-abraham <arashebi777@gmail.com>
2025-10-20 19:43:47 +0330

    Starting to implement the routing system

--- system/Router/Api/Api.php
+++ system/Router/Api/Api.php
@@ -1,5 +1,47 @@
 <?php
 
     namespace System\Router\Api;
+    
+    class Route{
+        public static function get($url,$executeMethod,$name=NULL) {
+            $executeMethod = explode("@",$executeMethod);
 
+            $class = $executeMethod[0];
+            $method = $executeMethod[1];
+
+            global $routes;
+
+            array_push($routes['get'],array('url' => trim($url,'/ '),'class' => $class,'method'=> $method , 'name' => $name));
+        }
+        public static function post($url,$executeMethod,$name=NULL) {
+            $executeMethod = explode("@",$executeMethod);
+
+            $class = $executeMethod[0];
+            $method = $executeMethod[1];
+
+            global $routes;
+
+            array_push($routes['post'],array('url' => trim($url,'/ '),'class' => $class,'method'=> $method , 'name' => $name));
+        }
+        public static function put($url,$executeMethod,$name=NULL) {
+            $executeMethod = explode("@",$executeMethod);
+
+            $class = $executeMethod[0];
+            $method = $executeMethod[1];
+
+            global $routes;
+
+            array_push($routes['put'],array('url' => trim($url,'/ '),'class' => $class,'method'=> $method , 'name' => $name));
+        }
+        public static function delete($url,$executeMethod,$name=NULL) {
+            $executeMethod = explode("@",$executeMethod);
+
+            $class = $executeMethod[0];
+            $method = $executeMethod[1];
+
+            global $routes;
+
+            array_push($routes['delete'],array('url' => trim($url,'/ '),'class' => $class,'method'=> $method , 'name' => $name));
+        }
+    }
 ?>
