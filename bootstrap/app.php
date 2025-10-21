7530025360d69a9da7c8bbc4c1eda0138262e2a4
Arash-abraham <arashebi777@gmail.com>
2025-10-21 12:27:18 +0330

    Starting to implement the routing system

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,5 +2,9 @@
 
     require_once("../config/app.php");
     require_once("../config/database.php");
+    
+    require_once("../routes/web.php");
+    require_once("../routes/api.php");
 
+    
 ?>
