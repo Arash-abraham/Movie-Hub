ca1c05dd6b97db2743677a85cd5c803457396fdd
Arash-abraham <arashebi777@gmail.com>
2025-10-24 19:21:10 +0330

    using traits in ORM

--- system/Database/ORM/Model.php
+++ system/Database/ORM/Model.php
@@ -2,4 +2,12 @@
 
     namespace System\Database\ORM;
 
+    use System\Database\Traits\HasAttributes;
+    use System\Database\Traits\HasCRUD;
+    use System\Database\Traits\HasMethodCaller;
+    use System\Database\Traits\HasQueryBuilder;
+    use System\Database\Traits\HasRelation;
+    use System\Database\Traits\HasTimestamps;
+    use System\Database\Traits\HasSoftDelete; 
+    
 ?>
