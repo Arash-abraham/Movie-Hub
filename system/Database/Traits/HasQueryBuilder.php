6e623183fa1a3c6cd6987ced573c0a29d72c0182
Arash-abraham <arashebi777@gmail.com>
2025-10-24 22:10:03 +0330

    make HasQueryBuilder

--- system/Database/Traits/HasQueryBuilder.php
+++ system/Database/Traits/HasQueryBuilder.php
@@ -7,7 +7,23 @@
     trait HasQueryBuilder {
         private $sql = "";
         protected $where = []; 
-        
+        private $orderBy = [];
+        private $limit = [];
+        private $values = [];
+        private $bindValues = [];
+
+        protected function setSql($query) {
+            $this->sql = $query;
+        }
+
+        protected function getSql() {
+            return $this->sql;
+        }
+
+        protected function resetSql() {
+            $this->sql = "";
+        }
+
     }
 
 
