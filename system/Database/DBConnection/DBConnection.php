543b95f932fcaaa012744638ff8c925212df2871
Arash-abraham <arashebi777@gmail.com>
2025-10-24 16:33:56 +0330

    Creating a dbConnection method to connect to a database with PDO\

--- system/Database/DBConnection/DBConnection.php
+++ system/Database/DBConnection/DBConnection.php
@@ -21,5 +21,12 @@
             return self::$dbConnectionInstance;
         }
 
+        private function dbConnection() {
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ];
+            
+        }
     }
 ?>
