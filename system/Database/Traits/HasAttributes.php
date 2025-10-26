1450495bbe60ca1ebed46e7e9f17ee7c29c69c20
Arash-abraham <arashebi777@gmail.com>
2025-10-26 18:09:17 +0330

    make HasAttributes

--- system/Database/Traits/HasAttributes.php
+++ system/Database/Traits/HasAttributes.php
@@ -3,7 +3,37 @@
     namespace System\Database\Traits;
 
     trait HasAttributes {
+        private function registerAttribut() {
+
+        }
+
+        protected function arrayToAttribut() {
+
+        }
+
+        protected function arrayToObject() {
+
+        }
         
+        private function inHiddenAttribut() {
+
+        }
+
+        private function inCastsAttribute() {
+
+        }
+
+        private function castDecodeValue() {
+
+        }
+
+        private function castEncodeValue() {
+            
+        }
+
+        private function arrayToCastEncodeValue() {
+            
+        }
     }
 
 ?>
