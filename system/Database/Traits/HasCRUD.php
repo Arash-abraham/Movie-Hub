72dac5227b5ae0da95e0e726398914ee198f5c36
Arash-abraham <arashebi777@gmail.com>
2025-10-28 13:09:32 +0330

    guarded returns to the code, this method will be written! | end

--- system/Database/Traits/HasCRUD.php
+++ system/Database/Traits/HasCRUD.php
@@ -4,22 +4,37 @@
     use System\Database\DBConnection\DBConnection;
 
     trait HasCRUD {
+
+        public function saveMethod(){
+
+        }
+
         protected function fill() {
             $fillArray = [];
+            
             foreach ($this->fillable as $attribute) {
+                if ($this->inGuardedAttribute($attribute)) {
+                    continue;
+                }
+                
                 if(isset($this->$attribute)) {
                     array_push($fillArray, $this->getAttributeName($attribute) . " = ?");
                     $this->inCastsAttribute($attribute) == true
                         ?
-                            $this->addValue($attribute , $this->castEncodeValue($attribute , $this->$attribute))
+                            $this->addValue($attribute, $this->castEncodeValue($attribute, $this->$attribute))
                         :
-                            $this->addValue($attribute , $this->$attribute);
+                            $this->addValue($attribute, $this->$attribute);
                 }
             }
-
+        
             $fillString = implode(", ", $fillArray);
             return $fillString;
         }
+        
+        private function inGuardedAttribute($attribute) {
+            return !empty($this->guarded) && in_array($attribute, $this->guarded);
+        }
+        
         // public function guard() {
         //     $guard = [];
         // }
