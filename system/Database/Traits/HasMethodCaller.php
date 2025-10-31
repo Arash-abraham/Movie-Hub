5f59c47a9a2060add60fe086c98ee53ca50a9502
Arash-abraham <arashebi777@gmail.com>
2025-10-31 19:02:15 +0330

    Getting Started with the HasMethodCaller Method Implementation

--- system/Database/Traits/HasMethodCaller.php
+++ system/Database/Traits/HasMethodCaller.php
@@ -24,7 +24,7 @@
             $methodName = $method.$suffix;
 
             if (in_array($method , $this->allMethods)) {
-                
+                call_user_func_array([$object , $methodName], $args);
             }
         }
 
