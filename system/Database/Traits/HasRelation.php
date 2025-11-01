128f5a7d2edc27e01d197a1f64e58a338b683410
Arash-abraham <arashebi777@gmail.com>
2025-11-01 22:22:48 +0330

    Development of HasRelation

--- system/Database/Traits/HasRelation.php
+++ system/Database/Traits/HasRelation.php
@@ -4,13 +4,12 @@
 
     trait HasRelation {
         protected function hasOne($model , $foreignKey , $localKey) {
-
+            if($this->{$this->primaryKey}) {
+                $modelObject = new $model;
+            }
         }
 
         public function getHasRelation() {
-            if($this->{$this->primaryKey}) {
-
-            }
         }
 
     }
