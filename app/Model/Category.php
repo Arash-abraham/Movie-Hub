fd286468e54a0985a0e34e22d9e6dba2d866b09d
Arash-abraham <arashebi777@gmail.com>
2025-11-04 12:57:33 +0330

    Test Model

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -1,4 +1,15 @@
 <?php 
 
+    namespace App;
 
+    use System\Database\ORM\Model;
+    
+    class Category extends Model {
+        protected $table = "categories";
+        protected $fillable = [
+            "name"
+        ];
+        protected $guarded = ["id"];
+        protected $casts = [];
+    }
 ?>
