b89f5847d0dd1c4e92bb09ac0b4a9c76b778c27a
Arash-abraham <arashebi777@gmail.com>
2025-11-04 12:59:44 +0330

    Test Model

--- app/Model/Role.php
+++ app/Model/Role.php
@@ -1,3 +1,15 @@
 <?php 
 
+    namespace App;
+
+    use System\Database\ORM\Model;
+    
+    class Role extends Model {
+        protected $table = "roles";
+        protected $fillable = [
+            "name"
+        ];
+        protected $guarded = ["id"];
+        protected $casts = [];
+    }
 ?>
