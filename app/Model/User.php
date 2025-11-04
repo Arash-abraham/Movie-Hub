842641b1656414069f3dfa203d4ac70d40d82399
Arash-abraham <arashebi777@gmail.com>
2025-11-04 12:58:56 +0330

    Test Model

--- app/Model/User.php
+++ app/Model/User.php
@@ -1,6 +1,15 @@
-<?php
+<?php 
 
     namespace App;
-    
 
+    use System\Database\ORM\Model;
+    
+    class User extends Model {
+        protected $table = "users";
+        protected $fillable = [
+            "username"
+        ];
+        protected $guarded = ["id"];
+        protected $casts = [];
+    }
 ?>
