92403fc9ece13b7c91d615d3f6e6940791a6107f
Arash-abraham <arashebi777@gmail.com>
2025-11-05 23:33:54 +0330

    A start on the development of migrations

--- system/Database/DBBuilder/DBBuilder.php
+++ system/Database/DBBuilder/DBBuilder.php
@@ -5,7 +5,12 @@
     
     class DBBuilder {
         public function __construct() {
-            
+            $this->createTables();
+            die('Migrations run successfully !');
+        }
+
+        protected function createTables() {
+
         }
     }
 ?>
