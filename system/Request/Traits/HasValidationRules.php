7842f41276165b730b0d7f0fa34f86af5f2e60de
Arash-abraham <arashebi777@gmail.com>
2025-11-06 20:36:58 +0330

    HasValidationRules trait development

--- system/Request/Traits/HasValidationRules.php
+++ system/Request/Traits/HasValidationRules.php
@@ -34,6 +34,34 @@
                 }
             }
         }
+        public function numberValidation($name, $ruleArray) {
+            foreach ($ruleArray as $rule) {
+                if ($rule == 'required') {
+                    $this->required($name);
+                }
+                else if (strpos($rule,'max:') === 0) {
+                    $rule = str_replace('max:','', $rule);
+                    $this->maxNumber($name, $rule);
+                }
+                else if (strpos($rule,'min:') === 0) {
+                    $rule = str_replace('min:','', $rule);
+                    $this->minNumber($name, $rule);
+                }
+                else if (strpos($rule,'exists:') === 0) {
+                    $rule = str_replace('exists:','', $rule);
+                    $rule = explode(',', $rule);
+                    $kry = isset($rule[1]) == false
+                        ? 
+                            NULL
+                        : 
+                            $rule[1];
+                    $this->existsIn($name, $rule[0], $kry);
+                }
+                else if($rule == 'number') {
+                    $this->number($name);
+                }
+            }
+        }
     }
 
 ?>
