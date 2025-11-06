c4fa8b81ae213d8de652261718460f938c104f86
Arash-abraham <arashebi777@gmail.com>
2025-11-06 12:12:10 +0330

    Implementation __construct

--- system/Request/Request.php
+++ system/Request/Request.php
@@ -2,11 +2,21 @@
 
     namespace System\Request;
 
-    use System\Request\Traits\HasFileValidationRules;
+use Illuminate\Testing\Fluent\Concerns\Has;
+use System\Request\Traits\HasFileValidationRules;
     use System\Request\Traits\HasRunValidation;
     use System\Request\Traits\HasValidationRules;
 
     class Request {
-        
+        use HasFileValidationRules, HasRunValidation, HasValidationRules;
+
+        protected $errorExist = false;
+        protected $request;
+        protected $files = NULL;
+        protected $errorVariablesName = [];
+
+        public function __construct() {
+
+        }
     }
 ?>
