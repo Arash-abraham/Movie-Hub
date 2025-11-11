47448c785d8f37b0801f8393287fee17d1b3962c
Arash-abraham <arashebi777@gmail.com>
2025-11-11 19:03:41 +0330

    And now it's time for the Auth class.

--- system/Auth/Auth.php
+++ system/Auth/Auth.php
@@ -6,5 +6,10 @@
     use System\Session\Session; 
 
     // Before developing the Auth class, Session must first be developed.
+    // The development of the Session class has been completed beautifully :)
+
+    class Auth {
+        
+    }
     
 ?>
