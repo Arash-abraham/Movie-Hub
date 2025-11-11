b44d17be137d9f61fdf3a0a625a4ad31f2a10a14
Arash-abraham <arashebi777@gmail.com>
2025-11-11 18:53:55 +0330

    The implementation of getter and setter methods is complete.

--- system/Session/Session.php
+++ system/Session/Session.php
@@ -7,8 +7,11 @@
             $_SESSION[$session_name] = $session_value;
         }
 
-        public function get() {
-
+        public function get($session_name) {
+            if (isset($_SESSION[$session_name])) {
+                return $_SESSION[$session_name];
+            }
+            return false;
         }
 
         public function remove() {
