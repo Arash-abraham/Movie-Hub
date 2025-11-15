73a4b6d1c006fcb0f3ee83cf94c6784317987c66
Arash-abraham <arashebi777@gmail.com>
2025-11-15 17:17:39 +0330

    HasViewLoader development process

--- system/View/Traits/HasViewLoader.php
+++ system/View/Traits/HasViewLoader.php
@@ -3,7 +3,15 @@
     namespace System\View\Traits;
 
     trait HasViewLoader {
-        
+        private $viewNameArray = [];
+
+        private function ViewLoader() {
+
+        }
+
+        private function registerView($name) {
+
+        }
     }
 
 ?>
