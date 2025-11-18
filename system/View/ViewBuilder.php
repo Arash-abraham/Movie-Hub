8c53dc9c88b24c4e246744fd3043447846eaf3a0
Arash-abraham <arashebi777@gmail.com>
2025-11-18 17:40:14 +0330

    Includes in twig

--- system/View/ViewBuilder.php
+++ system/View/ViewBuilder.php
@@ -4,16 +4,18 @@
 
     use System\View\Traits\HasViewLoader;
     use System\View\Traits\HasExtendContent;
+    use System\View\Traits\HasIncludeContent;
     
 
     class ViewBuilder {
-        use HasViewLoader , HasExtendContent;
+        use HasViewLoader , HasExtendContent , HasIncludeContent;
 
         public $content;
 
         public function run($dir) {
             $this->content = $this->viewLoader($dir);
             $this->checkExtendContent();
+            $this->checkIncludeContent();
         }
     }
 
