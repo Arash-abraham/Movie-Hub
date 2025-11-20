8d573a08e8016c12d7ea479f8f00dbbf8f68b70c
Arash-abraham <arashebi777@gmail.com>
2025-11-20 10:52:00 +0330

    testing

--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?php
-    $content = '{% block test %}test{% endblock %}';
-    
-    preg_match_all("/\s*{%\s+block\s+([\w_]+)\s*%}(.*?){%\s+endblock\s+\\1\s*%}/s", $content, $matches, PREG_UNMATCHED_AS_NULL);
-    
+    $content = '{% block  sd %}
+                        <h1>hello</h1>
+                {% endblock %} sadsadsadsasadsaddsa';
+
+    preg_match_all("/\s*{%\s+block\s+([\w_]+)\s*%}(.*?){%\s+endblock\s*%}/s", $content, $matches, PREG_UNMATCHED_AS_NULL);
+
     print_r($matches);
 ?>
