a4e530783b4b03ff884ec1656ca2f7f0564826ed
Arash-abraham <arashebi777@gmail.com>
2025-11-22 19:05:19 +0330

    writing tests

--- system/Helper/helper.php
+++ system/Helper/helper.php
@@ -5,13 +5,7 @@
         $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . DIRECTORY_SEPARATOR . 'system' . DIRECTORY_SEPARATOR . 'Router' . DIRECTORY_SEPARATOR . 'View');
         $twig = new \Twig\Environment($loader);
         
-        $data = [
-            'page_title' => "Page Not Found - ".APP_TITLE,
-            'error_code' => "404",
-            'error_title' => "Oops! Page Not Found",
-            'message' => "The page you're looking for doesn't exist or has been moved.",
-            'home_url' => BASE_URL
-        ];
+        $data = $vars;
         
         echo $twig->render($dir, $data);
         exit;
