5448ce7059a0b9fba10acc98a3c144f2ae8bfd8e
Arash-abraham <arashebi777@gmail.com>
2025-11-22 18:59:15 +0330

    Continuing the helper development process along with writing tests

--- system/Helper/helper.php
+++ system/Helper/helper.php
@@ -2,6 +2,18 @@
     use System\View\ViewBuilder;
 
     function view($dir , $vars = []) {
-        $viewBuilder = new ViewBuilder();
+        $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . DIRECTORY_SEPARATOR . 'system' . DIRECTORY_SEPARATOR . 'Router' . DIRECTORY_SEPARATOR . 'View');
+        $twig = new \Twig\Environment($loader);
+        
+        $data = [
+            'page_title' => "Page Not Found - ".APP_TITLE,
+            'error_code' => "404",
+            'error_title' => "Oops! Page Not Found",
+            'message' => "The page you're looking for doesn't exist or has been moved.",
+            'home_url' => BASE_URL
+        ];
+        
+        echo $twig->render($dir, $data);
+        exit;
     }
 ?>
--- test.php
+++ test.php
@@ -6,4 +6,21 @@
     // preg_match_all("/\s*{%\s+block\s+([\w_]+)\s*%}(.*?){%\s+endblock\s*%}/s", $content, $matches, PREG_UNMATCHED_AS_NULL);
 // 
     // print_r($matches);
+
+    // $loader = __DIR__ . DIRECTORY_SEPARATOR . 'View';
+    // print $loader;
+    // require_once 'vendor/autoload.php';
+
+    // $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . DIRECTORY_SEPARATOR . 'system' . DIRECTORY_SEPARATOR . 'Router' . DIRECTORY_SEPARATOR . 'View');
+    // $twig = new \Twig\Environment($loader);
+    // $data = [
+    //     'page_title' => "Page Not Found - ",
+    //     'error_code' => "404",
+    //     'error_title' => "Oops! Page Not Found",
+    //     'message' => "The page you're looking for doesn't exist or has been moved.",
+    //     'home_url' => 'test'
+    // ];
+    // echo $twig->render('404.twig', $data);
+
+    // print __DIR__ . DIRECTORY_SEPARATOR . 'System' . DIRECTORY_SEPARATOR . 'Router' . DIRECTORY_SEPARATOR . 'View';
 ?>
