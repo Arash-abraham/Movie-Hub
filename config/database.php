439a4876611452945ca50ba2b0bb0eba1d419cd0
Arash-abraham <arashebi777@gmail.com>
2025-11-23 20:26:25 +0330

    Using .env to configure the database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,21 @@
 <?php 
 
+    // require_once 'vendor/autoload.php';
 
+    use Dotenv\Dotenv;
+
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+
+    $dotenv->required(['APP_TITLE', 'BASE_URL']);
+
+    define("DB_HOST", $_ENV['DBHOST']);
+    define("DB_NAME", $_ENV['DBNAME']);
+    define("DB_USERNAME", $_ENV['DBUSERNAME']);
+    define("DB_PASSWORD", $_ENV['DBPASSWORD'] ?? ''); 
+
+    if ($_ENV['APP_DEBUG'] ?? false) {
+        error_log("Database Config - Host: " . DB_HOST . ", DB: " . DB_NAME);
+    }
 
 ?>
