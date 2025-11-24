38e03c2e77d77df86ff4b842f398fa7501042bcd
Arash-abraham <arashebi777@gmail.com>
2025-11-24 12:20:56 +0330

    Rewriting the configuration section by extending the Config class

--- config/Config.php
+++ config/Config.php
@@ -4,6 +4,13 @@
         private static $instance;
         private $config_nested_array = [];
         private $config_dot_array = [];
+
+        private function __construct() {
+            $this->initialConfigArrays();
+        }
+        private function initialConfigArrays() {
+            
+        }
     }
 
 ?>
