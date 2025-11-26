68824e6daa862f6889e8ff10d0001cf6320948d2
Arash-abraham <arashebi777@gmail.com>
2025-11-26 20:04:22 +0330

    Fixing view rendering bugs with twig :)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,7 @@
         public function index() {
             return view('app',[
                 'name' => 'arash'
-            ]);
+            ]); // worked :)
         }
 
         public function create() {
